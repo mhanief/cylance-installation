778f7ba219d2ddc9cf11d8661ffc6a4af4aba951
mhanief <47133144+mhanief@users.noreply.github.com>
2022-04-08 12:50:06 +0800

    Create install-cylance.sh

---
+++ install-cylance.sh
@@ -0,0 +1,10 @@
+mkdir /opt/cylance/
+touch /opt/cylance/config_defaults.txt
+
+echo InstallToken=jgQnIw9fl88XMTjLPOM9ebsb > /opt/cylance/config_defaults.txt
+echo SelfProtectionLevel=2 >> /opt/cylance/config_defaults.txt
+echo LogLevel=2 >> /opt/cylance/config_defaults.txt
+echo VenueZone=PayANZ >> /opt/cylance/config_defaults.txt
+echo UiMode=2 >> /opt/cylance/config_defaults.txt
+
+yum install CylancePROTECT.el7.rpm
